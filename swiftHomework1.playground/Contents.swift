6b2a83f3b9ab033b8dce30a4041902a9151854f5
CalvinChuo <63452906+CalvinChuo@users.noreply.github.com>
2020-12-25 11:19:59 +0800

    First commit

---
+++ swiftHomework1.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+//This is a homework to write the characteristic of my favourite song with swift
+
+var Artist : String = "Lady Gaga and Bradley Cooper"
+var SongName : String = "Shallow"
+var YearReleased : Int = 2018
+var Genre : String = "Country Music"
+var Duration : Float = 3.37
+var Album : String = "A Star Is Born"
+
+print(Album)
+print(Artist)
+print(Duration)
+print(Genre)
+print(SongName)
+print(YearReleased)
